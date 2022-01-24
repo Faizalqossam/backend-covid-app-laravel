3f6145cadcc89491fa606c6220cefeb0f2353d33
Faizalqossam <72482067+Faizalqossam@users.noreply.github.com>
2022-01-24 20:40:37 +0700

    Update PatiensController.php

--- app/Http/Controllers/PatiensController.php
+++ app/Http/Controllers/PatiensController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 # import model patiens untuk menggunakan elequent
-use App\Models\patiens;
+use App\Models\Patiens;
 use Illuminate\Http\Request;
 
 class PatiensController extends Controller
